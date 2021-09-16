b78ea00b4204fb6bae99e0ae33d1a394eb3bb9a1
Fabricio8191 <89555630+Fabricio8191@users.noreply.github.com>
2021-09-16 02:52:12 -0300

    Add files via upload

---
+++ cadastroDeEventos.js
@@ -0,0 +1,26 @@
+var dataDeHoje = "Setembro 16, 2021";
+
+var dataDoEvento = "Janeiro 5, 2022"; {
+    if (dataDoEvento <= dataDeHoje)
+    console.log("Data Invalida");
+    else {
+        console.log ("Data Permitida")
+    }
+
+}
+
+var idadeLimite = "18 anos"; {
+    if(idadeLimite => "18 anos")
+    console.log("Permitido")
+    else
+    console.log("Você Prescisa Ser Maior De Idade")
+}
+    
+
+var maximoDeParticipantes = "100"; {
+    if(maximoDeParticipantes <= "100")
+    console.log("Numero De Participantes Valido")
+    else
+    console.log("Você Excedeu o Número De Participantes")
+}
+
